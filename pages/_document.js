1ac489d26bd854a94cd58157f1c35bd7b5419017
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2022-06-17 14:25:10 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
                 <Head>
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
+                        data-cookiecategory="analytics"
                         async
                         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
                     />
